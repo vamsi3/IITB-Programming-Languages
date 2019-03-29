551a62215c8c0483fa6e52e141b66c65bee22b71
Vamsi Krishna <vamsi.1999@outlook.com>
2019-03-30 01:35:00 +0530

    (Hopefully) Complete icode.cc excluding print statements

--- src/icode.cc
+++ src/icode.cc
@@ -21,6 +21,30 @@ Instruction_Descriptor::Instruction_Descriptor() {
 
 }
 
+Tgt_Op Instruction_Descriptor::get_op() {
+    return this->inst_op;
+}
+
+string Instruction_Descriptor::get_name() {
+    return this->mnemonic;
+}
+
+string Instruction_Descriptor::get_mnemonic() {
+    return this->ic_symbol;
+}
+
+string Instruction_Descriptor::get_ic_symbol() {
+    return this->name;
+}
+
+Icode_Format Instruction_Descriptor::get_ic_format() {
+    return this->ic_format;
+}
+
+Assembly_Format Instruction_Descriptor::get_assembly_format() {
+    return this->assem_format;
+}
+
 
 
 ///////////////////////////// Icode statement operand ///////////////////////////////////
@@ -38,15 +62,18 @@ Mem_Addr_Opd::Mem_Addr_Opd(Symbol_Table_Entry & se) {
 }
 
 void Mem_Addr_Opd::print_ics_opd(ostream & file_buffer) {
-
+    file_buffer << "[Mem_Addr_Opd][print_ics_opd]" << endl;
 }
 
 void Mem_Addr_Opd::print_asm_opd(ostream & file_buffer) {
-
+    file_buffer << "[Mem_Addr_Opd][print_asm_opd]" << endl;
 }
 
 Mem_Addr_Opd & Mem_Addr_Opd::operator=(const Mem_Addr_Opd & rhs) {
-
+    if (this != &rhs) {
+        *this->symbol_entry = *rhs.symbol_entry;
+    }
+    return *this;
 }
 
 
@@ -61,15 +88,20 @@ Register_Descriptor * Register_Addr_Opd::get_reg() {
 }
 
 void Register_Addr_Opd::print_ics_opd(ostream & file_buffer) {
+    file_buffer << "[Register_Addr_Opd][print_ics_opd]" << endl;
 
 }
 
 void Register_Addr_Opd::print_asm_opd(ostream & file_buffer) {
+    file_buffer << "[Register_Addr_Opd][print_asm_opd]" << endl;
 
 }
 
 Register_Addr_Opd & Register_Addr_Opd::operator=(const Register_Addr_Opd & rhs) {
-
+    if (this != &rhs) {
+        *this->register_description = *rhs.register_description;
+    }
+    return *this;
 }
 
 
@@ -82,17 +114,20 @@ Const_Opd<T>::Const_Opd(T num) {
 
 template <class T>
 void Const_Opd<T>::print_ics_opd(ostream & file_buffer) {
-
+    file_buffer << "[Const_Opd][print_ics_opd]" << endl;
 }
 
 template <class T>
 void Const_Opd<T>::print_asm_opd(ostream & file_buffer) {
-
+    file_buffer << "[Const_Opd][print_asm_opd]" << endl;
 }
 
 template <class T>
 Const_Opd<T> & Const_Opd<T>::operator=(const Const_Opd<T> & rhs) {
-
+    if (this != &rhs) {
+        this->num = rhs.num;
+    }
+    return *this;
 }
 
 
@@ -122,13 +157,18 @@ void Icode_Stmt::set_result(Ics_Opd * io) {}
 // ============ Move_IC_Stmt ===================================================
 
 Move_IC_Stmt::Move_IC_Stmt(Tgt_Op inst_op, Ics_Opd * opd1, Ics_Opd * result) {
-    // this->op_desc = new Instruction_Descriptor(inst_op, '', '', '', icf, af); // TODO!
+    this->op_desc = *machine_desc_object.spim_instruction_table[inst_op];
     this->opd1 = opd1;
     this->result = result;
 }
 
 Move_IC_Stmt & Move_IC_Stmt::operator=(const Move_IC_Stmt & rhs) {
-    
+    if (this != &rhs) {
+        this->op_desc = rhs.op_desc;
+        *this->opd1 = *rhs.opd1;
+        *this->result = *rhs.result;
+    }
+    return *this;
 }
 
 Instruction_Descriptor & Move_IC_Stmt::get_inst_op_of_ics() {
@@ -152,25 +192,31 @@ void Move_IC_Stmt::set_result(Ics_Opd * io) {
 }
 
 void Move_IC_Stmt::print_icode(ostream & file_buffer) {
-
+    file_buffer << "[Move_IC_Stmt][print_icode] " << this->op_desc.get_name() << endl;
 }
 
 void Move_IC_Stmt::print_assembly(ostream & file_buffer) {
-
+    file_buffer << "[Move_IC_Stmt][print_icode] " << this->op_desc.get_mnemonic() << endl;
 }
 
 
 // ============ Compute_IC_Stmt ================================================
 
 Compute_IC_Stmt::Compute_IC_Stmt(Tgt_Op inst_op, Ics_Opd * opd1, Ics_Opd * opd2, Ics_Opd * result) {
-    // this->op_desc = new Instruction_Descriptor(inst_op, '', '', '', icf, af); // TODO!
+    this->op_desc = *machine_desc_object.spim_instruction_table[inst_op];
     this->opd1 = opd1;
     this->opd2 = opd2;
     this->result = result;
 }
 
 Compute_IC_Stmt & Compute_IC_Stmt::operator=(const Compute_IC_Stmt & rhs) {
-
+    if (this != &rhs) {
+        this->op_desc = rhs.op_desc;
+        *this->opd1 = *rhs.opd1;
+        *this->opd2 = *rhs.opd2;
+        *this->result = *rhs.result;
+    }
+    return *this;
 }
 
 Instruction_Descriptor & Compute_IC_Stmt::get_inst_op_of_ics() {
@@ -202,24 +248,29 @@ void Compute_IC_Stmt::set_result(Ics_Opd * io) {
 }
 
 void Compute_IC_Stmt::print_icode(ostream & file_buffer) {
-
+    file_buffer << "[Compute_IC_Stmt][print_icode] " << this->op_desc.get_name() << endl;
 }
 
 void Compute_IC_Stmt::print_assembly(ostream & file_buffer) {
-
+    file_buffer << "[Compute_IC_Stmt][print_assembly] " << this->op_desc.get_mnemonic() << endl;
 }
 
 
 // ============ Control_Flow_IC_Stmt ===========================================
 
 Control_Flow_IC_Stmt::Control_Flow_IC_Stmt(Tgt_Op inst_op, Ics_Opd * opd1, string label) {
-    // this->op_desc = new Instruction_Descriptor(inst_op, '', '', '', icf, af); // TODO!
+    this->op_desc = *machine_desc_object.spim_instruction_table[inst_op];
     this->opd1 = opd1;
     this->label = label;
 }
 
 Control_Flow_IC_Stmt & Control_Flow_IC_Stmt::operator=(const Control_Flow_IC_Stmt & rhs) {
-
+    if (this != &rhs) {
+        this->op_desc = rhs.op_desc;
+        *this->opd1 = *rhs.opd1;
+        this->label = rhs.label;
+    }
+    return *this;
 }
 
 Instruction_Descriptor & Control_Flow_IC_Stmt::get_inst_op_of_ics() {
@@ -243,23 +294,27 @@ void Control_Flow_IC_Stmt::set_label(string label) {
 }
 
 void Control_Flow_IC_Stmt::print_icode(ostream & file_buffer) {
-
+    file_buffer << "[Control_Flow_IC_Stmt][print_icode] " << this->op_desc.get_name() << endl;
 }
 
 void Control_Flow_IC_Stmt::print_assembly(ostream & file_buffer) {
-
+    file_buffer << "[Control_Flow_IC_Stmt][print_assembly] " << this->op_desc.get_mnemonic() << endl;
 }
 
 
 // ============ Label_IC_Stmt ==================================================
 
 Label_IC_Stmt::Label_IC_Stmt(Tgt_Op inst_op, string label) {
-    // this->op_desc = new Instruction_Descriptor(inst_op, '', '', '', icf, af); // TODO!
+    this->op_desc = *machine_desc_object.spim_instruction_table[inst_op];
     this->label = label;
 }
 
 Label_IC_Stmt & Label_IC_Stmt::operator=(const Label_IC_Stmt & rhs) {
-
+    if (this != &rhs) {
+        this->op_desc = rhs.op_desc;
+        this->label = rhs.label;
+    }
+    return *this;
 }
 
 Instruction_Descriptor & Label_IC_Stmt::get_inst_op_of_ics() {
@@ -275,11 +330,11 @@ void Label_IC_Stmt::set_label(string label) {
 }
 
 void Label_IC_Stmt::print_icode(ostream & file_buffer) {
-
+    file_buffer << "[Label_IC_Stmt][print_icode] " << this->op_desc.get_name() << endl;
 }
 
 void Label_IC_Stmt::print_assembly(ostream & file_buffer) {
-
+    file_buffer << "[Label_IC_Stmt][print_assembly] " << this->op_desc.get_mnemonic() << endl;
 }
 
 
@@ -315,5 +370,9 @@ void Code_For_Ast::set_reg(Register_Descriptor * reg) {
 }
 
 Code_For_Ast & Code_For_Ast::operator=(const Code_For_Ast & rhs) {
-    
+    if (this != &rhs) {
+        this->ics_list = rhs.ics_list;
+        *this->result_register = *rhs.result_register;
+    }
+    return *this;
 }
